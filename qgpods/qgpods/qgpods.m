4e863538bf00691f826a06055388cc72b6fdf981
strong <strong@example.com>
2017-08-30 17:08:36 +0800

    修改tags号码

--- qgpods/qgpods/qgpods.m
+++ qgpods/qgpods/qgpods.m
@@ -16,22 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 34, 45)];
+    btn.backgroundColor = [UIColor redColor];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
